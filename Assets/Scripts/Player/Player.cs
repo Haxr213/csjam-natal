6679e27c46609dfa4a0f219cca4bc04baaa65852
Yhann Cruz <yhann.cruz@logicalminds.com.br>
2025-12-09 14:57:06 -0300

    feat: animação elfo

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,29 +4,150 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5f;
-    public Vector2 moveInput;
-    void Update()
+    [Header("References")]
+    [SerializeField] private LayerMask slotLayer;
+    [SerializeField] private Transform highlight;
+    [SerializeField] private GameObject[] turrets;
+
+    [Header("Attributes")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float tileSize = 1f;
+    [SerializeField] private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private Animator animator;
+    [SerializeField] private Vector2 lookDirection = Vector2.down;
+    private int tileLayer;
+    private int tileUnderPlayer;
+    private readonly string nameTileLayer = "Tile";
+    private readonly string nameTileUnderPlayer = "TileUnderPlayer";
+
+    private void Start()
     {
-        Move(moveInput);
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        highlight.gameObject.SetActive(false);
+        tileLayer = LayerMask.NameToLayer(nameTileLayer);
+        tileUnderPlayer = LayerMask.NameToLayer(nameTileUnderPlayer);
+    }
+
+    private void Update()
+    {
+        Move();
+        SetAnimationWalking();
+        UpdateSelection();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        setTileUnderPlayerLayer(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        setTileLayer(other);
     }
 
     public void ReadMoveInput(InputAction.CallbackContext context)
     {
+        if (context.canceled)
+        {
+            SetAnimationIdle();
+        }
+
         moveInput = context.ReadValue<Vector2>();
     }
 
-    private void Move(Vector2 direction)
+    public void ReadLookInput(InputAction.CallbackContext context)
     {
-        Vector3 movement = new Vector3(direction.x, direction.y, 0f);
-        transform.Translate(movement * speed * Time.deltaTime);
+        if (context.canceled)
+        {
+            SetAnimationIdle();
+            return;
+        }
+        SetAnimationIdle();
+        lookDirection = context.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0f);
+        rb.linearVelocity = movement * speed;
     }
 
     public void ReadActionInput(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Debug.Log("Action performed!");
+            SpawnTurret();
         }
+    }
+
+    private void SpawnTurret()
+    {
+        GameObject turretToSpawn = turrets[0];
+        if (highlight.gameObject.activeSelf)
+        {
+            Instantiate(turretToSpawn, highlight.position, Quaternion.identity);
+        }
+        
+
+    }
+    private void SetAnimationWalking()
+    {
+        bool isWalking = moveInput.magnitude > 0;
+        if (isWalking)
+        {
+            lookDirection = moveInput;
+            animator.SetBool("IsWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+    }
+
+    private void SetAnimationIdle()
+    {
+        if (moveInput.magnitude > 0)
+        {
+            lookDirection = moveInput;
+        }
+        animator.SetBool("IsWalking", false);
+        animator.SetFloat("LastInputX", lookDirection.x);
+        animator.SetFloat("LastInputY", lookDirection.y);
+    }
+
+    private void UpdateSelection()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            lookDirection,
+            tileSize,
+            slotLayer
+        );
+
+        if (hit.collider != null)
+        {
+            highlight.gameObject.SetActive(true);
+            highlight.position = hit.collider.transform.position;
+        }
+        else
+        {
+            highlight.gameObject.SetActive(false);
+        }
+    }
+
+    private void setTileLayer(Collider2D collider)
+    {
+        setLayerCollider(collider, tileLayer);
+    }
+
+    private void setTileUnderPlayerLayer(Collider2D collider)
+    {
+        setLayerCollider(collider, tileUnderPlayer);
+    }
+
+    private void setLayerCollider(Collider2D collider, int layer)
+    {
+        if (collider.CompareTag(nameTileLayer))
+            collider.gameObject.layer = layer;
     }
 }
